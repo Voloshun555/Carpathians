273770d3bb57767ee803cca0f8b6839c3bc178ea
Voloshun <94966717+Voloshun555@users.noreply.github.com>
2026-01-01 21:24:32 +0200

    adit swiper

---
+++ src/js/swiper.js
@@ -0,0 +1,16 @@
+import Swiper from "swiper/bundle";
+
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
