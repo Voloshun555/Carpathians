391760de07a0a6eff40acbd7b9564edf9293ab45
Voloshun <94966717+Voloshun555@users.noreply.github.com>
2026-01-01 17:54:17 +0200

    add partials

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Carpathians/', // для GitHub Pages
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'partials'),
+    }),
+  ],
  
 })
