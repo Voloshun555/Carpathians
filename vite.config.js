54aedded4d41dd7da280ca5198866f433a4a1a79
Voloshun <94966717+Voloshun555@users.noreply.github.com>
2026-01-01 16:41:52 +0200

    add heder hero about advantages sections

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import handlebars from 'vite-plugin-handlebars'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Carpathians/', // для GitHub Pages
-  plugins: [
-    handlebars({
-      partialDirectory: resolve(__dirname, 'partials'),
-    }),
-  ],
+ 
 })
