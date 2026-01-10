f31ca700c6d65fe803c0b9176384068fcf8f817a
Voloshun <94966717+Voloshun555@users.noreply.github.com>
2026-01-10 17:51:11 +0200

    adit

--- src/js/dataForUpcomingTours.js
+++ src/js/dataForUpcomingTours.js
@@ -1,26 +1,43 @@
 import bukovelMobile from "/images/Upcoming-tours/bukovelMobile.jpg";
-
+import bukovelMobile2x from "/images/Upcoming-tours/bukovelMobile@2x.jpg";
 import carpathionsMobile from "/images/Upcoming-tours/carpathionsMobile.jpg";
+import carpathionsMobile2x from "/images/Upcoming-tours/carpathionsMobile@2x.jpg";
 import hoverlaMobile from "/images/Upcoming-tours/hoverlaMobile.jpg";
+import hoverlaMobile2x from "/images/Upcoming-tours/hoverlaMobile@2x.jpg";
 
+import hoverlaTablet from "/images/Upcoming-tours/hoverlaTablet.jpg";
+import hoverlaTablet2x from "/images/Upcoming-tours/hoverlaTablet@2x.jpg";
+import carpathionsTablet2x from "/images/Upcoming-tours/carpathiansTablet@2x.jpg";
+import carpathionsTablet from "/images/Upcoming-tours/carpathiansTablet.jpg";
+import bukovelTablet from "/images/Upcoming-tours/bukovelTablet.jpg";
+import bukovelTablet2x from "/images/Upcoming-tours/bukovelTablet@2x.jpg";
 
 export const data = [
   {
     id: 1,
-    img: bukovelMobile,
+    mobile: bukovelMobile,
+    mobile2x: bukovelMobile2x,
+    tablet: bukovelTablet,
+    tablet2x: bukovelTablet2x,
     title: "Ski tour to Bukovel",
     text: "From UAH 7,499/person",
   },
   {
     id: 2,
-    img: carpathionsMobile,
+    mobile: carpathionsMobile,
+    mobile2x: carpathionsMobile2x,
+    tablet: carpathionsTablet,
+    tablet2x: carpathionsTablet2x,
     title: "Week in Carpathians",
     text: "From UAH 9,999/person",
   },
   {
     id: 3,
-    img: hoverlaMobile,
-    title: "Ascent to Hoverla",
+    mobile: hoverlaMobile,
+    mobile2x: hoverlaMobile2x,
+    tablet: hoverlaTablet,
+    tablet2x: hoverlaTablet2x,
+    title: "Ascent to Hoverla",
     text: "From UAH 4,499/person",
   },
 ];
@@ -30,16 +47,34 @@ document
   .insertAdjacentHTML("beforeend", renderUpcomingToursList(data));
 function renderUpcomingToursList(data) {
   return data
-    .map(({ id, img, title, text }) => {
+    .map(({ mobile, mobile2x, tablet, tablet2x, title, text }) => {
       return `
-  <li class="item_upcoming">
-  <img src='${img}' class="item_img"></img>
-  <div class="content_item_wraper">
-  <h3 class="item_title"> ${title} </h3>
-  <p class="item_text">${text}</p>
-  <button class="item_btn">MORE DETAILS</button></div>
-  </li>
-  `;
+        <li class="item_upcoming">
+          <picture>
+            <!-- Tablet screens -->
+            <source 
+              media="(min-width: 768px)" 
+              srcset="${tablet} 1x, ${tablet2x} 2x"
+            >
+            <!-- Mobile screens -->
+            <source 
+              media="(max-width: 767px)" 
+              srcset="${mobile} 1x, ${mobile2x} 2x"
+            >
+            <!-- fallback -->
+            <img 
+              src="${mobile}" 
+              alt="${title}" 
+              class="item_img"
+            />
+          </picture>
+          <div class="content_item_wraper">
+            <h3 class="item_title">${title}</h3>
+            <p class="item_text">${text}</p>
+            <button class="item_btn">MORE DETAILS</button>
+          </div>
+        </li>
+      `;
     })
     .join("");
 }
