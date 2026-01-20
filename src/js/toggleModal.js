32d35078f1f2e9db5fe038f19b069ac686261828
Voloshun <94966717+Voloshun555@users.noreply.github.com>
2026-01-20 20:35:14 +0200

    edit modal js

--- src/js/toggleModal.js
+++ src/js/toggleModal.js
@@ -1,43 +1,57 @@
-
 const openModalHeroBtn = document.querySelector(".btn-hero");
 const modalHero = document.querySelector("#modal-hero");
 const closeModalHeroBtn = document.querySelector(".btn-cross-modal");
 
-const openBurgerBtn = document.querySelector(".burger-btn");
+const toggleBurgerBtn = document.querySelector(".burger-btn");
 const burgerMenu = document.querySelector("#burger-menu");
+const iconOpenBurgerMenu = document.querySelector(".burger-icon--open");
+const iconCloseBurgerMenu = document.querySelector(".burger-icon--close");
 
 const body = document.body;
 
-/**
- * @param {HTMLElement} modal
- */
-
-function openTarget(modal) {
+function openModal(modal) {
   modal.classList.remove("is-hidden");
   body.classList.add("modal-open");
 }
-
-function closeModal() {
-  modalHero.classList.add("is-hidden");
+function closeModal(modal) {
+  modal.classList.add("is-hidden");
   body.classList.remove("modal-open");
 }
 
-openModalHeroBtn.addEventListener("click", () => openTarget(modalHero));
+function toggleModal(modal) {
+  const isHidden = modal.classList.contains("is-hidden");
+  modal.classList.toggle("is-hidden");
+  if (isHidden) {
+    body.classList.add("modal-open");
+    iconOpenBurgerMenu.classList.add("changeIcon");
+    iconCloseBurgerMenu.classList.remove("changeIcon");
+  } else {
+    body.classList.remove("modal-open");
+    iconOpenBurgerMenu.classList.remove("changeIcon");
+    iconCloseBurgerMenu.classList.add("changeIcon");
+  }
+}
+openModalHeroBtn.addEventListener("click", () => openModal(modalHero));
+closeModalHeroBtn.addEventListener("click", () => closeModal(modalHero));
 
-openBurgerBtn.addEventListener("click", () => openTarget(burgerMenu));
-
-closeModalHeroBtn.addEventListener("click", closeModal);
+toggleBurgerBtn.addEventListener("click", () => toggleModal(burgerMenu));
 
 modalHero.addEventListener("click", (event) => {
   if (event.target === modalHero) {
-    closeModal();
+    closeModal(modalHero);
   }
 });
 
 window.addEventListener("keydown", (event) => {
   if (event.code === "Escape") {
-    if (!modalHero.classList.contains("is-hidden")) {
-      closeModal();
-    }
+    closeModal(modalHero);
   }
+});
+
+window.matchMedia("(max-width: 768px)").addEventListener("change", () => {
+  burgerMenu.classList.add("is-hidden");
+  body.classList.remove("modal-open");
+
+  iconOpenBurgerMenu?.classList.remove("changeIcon");
+  iconCloseBurgerMenu?.classList.add("changeIcon");
 });
